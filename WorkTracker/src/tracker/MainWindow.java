0436a6318d74cdaa5d4db92c2e60ea554cc5cd75
Danail <danaildoganov@proton.me>
2026-02-02 21:55:51 +0100

    Update MainWindow.java

--- WorkTracker/src/tracker/MainWindow.java
+++ WorkTracker/src/tracker/MainWindow.java
@@ -3,6 +3,11 @@ package tracker;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainWindow {
 
@@ -38,6 +43,32 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(48, 25, 65, 22);
+		frame.getContentPane().add(comboBox);
+		
+		JLabel elapsedTime = new JLabel("Time elapsed");
+		elapsedTime.setBounds(284, 45, 89, 33);
+		frame.getContentPane().add(elapsedTime);
+		
+		JButton startButton = new JButton("Start");
+		startButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		startButton.setBounds(311, 141, 89, 23);
+		frame.getContentPane().add(startButton);
+		
+		JButton stopButton = new JButton("Stop");
+		stopButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		stopButton.setBounds(311, 175, 89, 23);
+		frame.getContentPane().add(stopButton);
+		
 
+	}
 }
